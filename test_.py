2603503ec17b4fe8de88e74a132dfde15ee975c3
quentin <quentin@pop-os.localdomain>
2019-03-03 18:44:34 +0100

    one green, one red

--- test_.py
+++ test_.py
@@ -1,5 +1,11 @@
 def func(x):
     return x + 1
 
+def ofunc(x):
+    return x + 2
+
 def test_anwser():
-    assert func(3) == 4
+    assert func(3) == 6
+
+def ofun_test():
+    assert ofunc(2) == 4
